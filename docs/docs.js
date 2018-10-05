ca02a078bcb06bd09fda38cff854474b6f35acf2
Andrew Morgan <andrew@amorgan.xyz>
2018-10-05 17:23:01 +0200

    Fix edit documentation links

--- docs/docs.js
+++ docs/docs.js
@@ -1,17 +1,3 @@
-$(function() {
-	//$("[role=contentinfo]").append("<a href='#loadComments' class='loadcomments'>Load comments</a>");
-	setTimeout(loadComments, 3000)
-	$(".rst-current-version").on("click", function() {
-		fixGithubLink($('.injected a:contains(View)'))
-		fixGithubLink($('.injected a:contains(Edit)'))
-	})
-})
-
-function fixGithubLink(elem) {
-	if (elem.length)
-		elem.attr("href", elem.attr("href").replace(/\/home\/docs\/checkouts\/readthedocs.org\/.*?\/latest/, ""))
-}
-
 function loadComments() {
 	$("[role=contentinfo]").append('<div id="disqus_thread"></div>')
 
