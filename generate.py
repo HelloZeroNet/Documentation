65e91ee2ba420390ad12c435e451c6fdeaa23eeb
rllola <rllola80@gmail.com>
2018-11-26 11:54:46 +0100

    Updated generate.py script

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import shutil
 from mkdocs.commands import build
 from mkdocs import config
 from mkdocs import exceptions
@@ -25,4 +26,26 @@ def build_doc(lang):
 
 for lang in languages:
     print lang
+    # Shallow list copy
+    tmp_languages = languages[:]
+    tmp_languages.remove(lang)
+    for l in tmp_languages:
+        # We have to do this way because there is no ignore option
+        shutil.move('./docs/' + l, './' + l)
     build_doc(lang)
+    for l in tmp_languages:
+        # We have to do this way because there is no ignore option
+        shutil.move('./' + l, './docs/' + l)
+
+# Move search/ to en/
+shutil.move('./site/search', './site/en/search')
+
+
+# Move everything needed in /site
+tmp_languages = languages[:]
+tmp_languages.remove('en')
+
+for lang in tmp_languages:
+    shutil.move('./site-' + lang + '/' + lang, './site/' + lang)
+    shutil.move('./site-' + lang + '/search', './site/' + lang + '/search')
+    shutil.rmtree('./site-'+ lang, ignore_errors=True)
